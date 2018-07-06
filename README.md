eb51889128aa9bbb73e0d78231e3bd4879dce189
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2018-07-06 11:32:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+frontend-nanodegree-arcade-game
+===============================
+This arcade game is the third project for Udacity's Front-End Web Developer Nanodegree Program, which focuses on mastery object-oriented Javascript. 
+
+## How to Run the Game
+
+To install the game, clone or download a local copy of the game. To play, simply open index.html from any web browser.
+
+Player must avoid bugs and get from the starting point to the water. 
+
+## How to Play the Game
+To play the game use up, down, left, and right arrow keys. When the player reaches the water, a pop-up window will appear. The game can be restarted.
+
+If a collision with bugs occurs, the game restarts.
+
+## Dependencies
+ - Original HTML file, resources.js, engine.js, app.js files provided by Udacity, Front End Web Development Course.
+
+- [Font Awesome](https://fontawesome.com/)
+- [Google Fonts](https://fonts.google.com/)
+
+-Special Thanks to Rodrick Bloomfield for a tutuorial: https://zoom.us/recording/play/aulotDlzKFegQFIJTaTzKgWvNkVsYtlwO454vL1UPE1Cm6lOUBQCtfVurPOIAGAS?startTime=1529542978000
+
+
---
+++ js/app.js
@@ -0,0 +1,111 @@
+let modal = document.querySelector('#modal');
+let playAgain = document.querySelector('.playAgain'); 
+
+class Entity {
+    constructor() {
+        this.sprite = 'images/'; 
+    }
+
+    // Draw the entity on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 83);
+    }
+
+    checkCollisions(character) {
+        if (this.y === character.y) {
+            if (this.x >= character.x - 0.9 && this.x <= character.x + 0.9) {
+                return true;
+            }
+        }
+        else {
+            return false;
+        }
+    }
+
+}
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+class Player extends Entity {
+    constructor() {
+        super();
+        this.sprite += 'char-boy.png'
+        this.moving = false;
+        this.win = false;
+        this.x = 2;
+        this.y = 5;
+    }
+
+    handleInput(input) {
+        switch(input) {
+            case 'left':
+                this.x = this.x > 0 ? this.x - 1 : this.x;
+                break;
+            case 'up':
+                this.y = this.y > 0 ? this.y - 1 : this.y;
+                break;
+            case 'right':
+                this.x = this.x < 4 ? this.x + 1 : this.x;
+                break;
+            case 'down':
+                this.y = this.y < 5 ? this.y + 1 : this.y;
+                break;
+            default:
+                break;
+        }
+        this.moving = true;
+    }
+
+    // When image re-renders player isn't moving
+    render() {
+        super.render();
+        this.moving = false;
+    }
+}
+
+// Enemies our player must avoid
+   // Variables applied to each of our instances go here
+class Enemy extends Entity {
+    constructor(x, y, speed) {
+        super();
+        this.sprite += 'enemy-bug.png';
+        this.speed = Math.random() * (5 - 2) + 2;
+        this.x = x;
+        this.y = y;
+    }
+
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt) { 
+        if (this.x < 5) {
+            // Update the enemy's position, required method for game
+            // Parameter: dt, a time delta between ticks
+            this.x += this.speed * dt;
+        }
+        else {
+            this.x = -1;;
+        }
+    }
+}
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+const player = new Player();
+const allEnemies = [...Array(3)].map((_,i)=> new Enemy(0,i+1)); 
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
+ 
+
